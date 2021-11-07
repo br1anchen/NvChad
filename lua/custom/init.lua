4ae9012bfafc3e9fcf831644250d2c50d0c45445
Brian Chen <brianchen8990@gmail.com>
2021-11-07 09:36:01 +0100

    Add custom config

---
+++ lua/custom/init.lua
@@ -0,0 +1,59 @@
+-- This is where your custom modules and plugins go.
+-- See the wiki for a guide on how to extend NvChad
+
+local hooks = require "core.hooks"
+
+-- NOTE: To use this, make a copy with `cp example_init.lua init.lua`
+
+--------------------------------------------------------------------
+
+-- To modify packaged plugin configs, use the overrides functionality
+-- if the override does not exist in the plugin config, make or request a PR,
+-- or you can override the whole plugin config with 'chadrc' -> M.plugins.default_plugin_config_replace{}
+-- this will run your config instead of the NvChad config for the given plugin
+
+-- hooks.override("lsp", "publish_diagnostics", function(current)
+--   current.virtual_text = false;
+--   return current;
+-- end)
+
+-- To add new mappings, use the "setup_mappings" hook,
+-- you can set one or many mappings
+-- example below:
+
+hooks.add("setup_mappings", function(map)
+
+    local opts = {noremap = true, silent = true, nowait = false}
+    -- map("n", "<leader>cc", "gg", opt) -- example to delete the buffer
+    map("n", "<leader>qq", "<cmd>wqa<cr>", opts)
+    map("n", "<leader>R", "<cmd>checktime<cr>", opts)
+
+    map("n", "<leader>gl", "<cmd>LazyGit<cr>", opts)
+
+    map("n", "<leader>ts", ":Telescope grep_string<CR>", opts)
+
+    map("n", "<leader>v<", "<C-w><", opts)
+    map("n", "<leader>v>", "<C-w>>", opts)
+    map("n", "<leader>v-", "<C-w>-", opts)
+    map("n", "<leader>v+", "<C-w>+", opts)
+    map("n", "<leader>v=", "<C-w>=", opts)
+    map("n", "<leader>vs", "<cmd>split<cr>", opts)
+    map("n", "<leader>vv", "<cmd>vsplit<cr>", opts)
+    map("n", "<leader>vd", "<cmd>close<cr>", opts)
+end)
+
+-- To add new plugins, use the "install_plugin" hook,
+-- NOTE: we heavily suggest using Packer's lazy loading (with the 'event' field)
+-- see: https://github.com/wbthomason/packer.nvim
+-- examples below:
+
+hooks.add("install_plugins", function(use)
+   use {
+      "kdheepak/lazygit.nvim",
+   }
+end)
+
+-- alternatively, put this in a sub-folder like "lua/custom/plugins/mkdir"
+-- then source it with
+
+-- require "custom.plugins.mkdir"
