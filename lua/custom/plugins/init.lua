b286f893e797af80a67131e211f6035163fecc84
Brian Chen <brianchen8990@gmail.com>
2022-03-07 20:07:45 +0100

    Fix aerial config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -196,13 +196,19 @@ return {
 
    {
       "stevearc/aerial.nvim",
-      after = "nvim-lspconfig",
       config = function()
-         local map = require("core.utils").map
-         map("n", "<leader>a", "<cmd>AerialToggle!<CR>")
-         map("n", "{", "<cmd>AerialPrev<CR>")
-         map("n", "[[", "<cmd>AerialPrevUp<CR>")
-         map("n", "]]", "<cmd>AerialNextUp<CR>")
+         require("aerial").setup {
+            on_attach = function(bufnr)
+               -- Toggle the aerial window with <leader>a
+               vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>a", "<cmd>AerialToggle!<CR>", {})
+               -- Jump forwards/backwards with '{' and '}'
+               vim.api.nvim_buf_set_keymap(bufnr, "n", "{", "<cmd>AerialPrev<CR>", {})
+               vim.api.nvim_buf_set_keymap(bufnr, "n", "}", "<cmd>AerialNext<CR>", {})
+               -- Jump up the tree with '[[' or ']]'
+               vim.api.nvim_buf_set_keymap(bufnr, "n", "[[", "<cmd>AerialPrevUp<CR>", {})
+               vim.api.nvim_buf_set_keymap(bufnr, "n", "]]", "<cmd>AerialNextUp<CR>", {})
+            end,
+         }
       end,
    },
 
