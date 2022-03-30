3722f60fa01d3d197840ce3211f453bd2675a5fa
Brian Chen <brianchen8990@gmail.com>
2022-03-30 10:11:20 +0200

    Fix dap configure for dart/flutter

--- lua/custom/plugins/dap-install.lua
+++ lua/custom/plugins/dap-install.lua
@@ -2,15 +2,26 @@ local M = {}
 
 function M.setup()
    local dap_install = require "dap-install"
+   local installation_path = vim.fn.stdpath "data" .. "/dapinstall/"
 
    dap_install.setup {
-      installation_path = vim.fn.stdpath "data" .. "/dapinstall/",
+      installation_path = installation_path,
    }
 
    local dbg_list = require("dap-install.api.debuggers").get_installed_debuggers()
 
+   local overrides = {
+      dart = {
+         adapters = {
+            type = "executable",
+            command = "node",
+            args = { installation_path .. "dart/Dart-Code/out/dist/debug.js", "flutter" },
+         },
+      },
+   }
+
    for _, debugger in ipairs(dbg_list) do
-      dap_install.config(debugger)
+      dap_install.config(debugger, overrides[debugger])
    end
 end
 
--- lua/custom/plugins/nvim-dap.lua
+++ lua/custom/plugins/nvim-dap.lua
@@ -1,27 +1,7 @@
 local map = require("core.utils").map
 local M = {}
-local dap_install = require "dap-install"
 
 function M.setup()
-   dap_install.config("dart", {
-      adapters = {
-         type = "executable",
-         command = "node",
-         args = { vim.fn.stdpath "data" .. "/dapinstall/dart-code/out/dist/debug.js", "flutter" },
-      },
-      configurations = {
-         {
-            type = "dart",
-            request = "launch",
-            name = "Launch flutter",
-            dartSdkPath = os.getenv "HOME" .. "/snap/flutter/common/flutter/bin/cache/dart-sdk/",
-            flutterSdkPath = os.getenv "HOME" .. "/snap/flutter/common/flutter",
-            program = "${workspaceFolder}/lib/main.dart",
-            cwd = "${workspaceFolder}",
-         },
-      },
-   })
-
    local opts = { noremap = true, silent = true, nowait = false }
    map("n", "<leader>zb", ":lua require'dap'.toggle_breakpoint()<cr>", opts)
    map("n", "<leader>zc", ":lua require'dap'.continue()<cr>", opts)
