70b5d5d03de8b09b19583550ea5a781249585eef
Brian Chen <brianchen8990@gmail.com>
2022-04-16 15:11:01 +0200

    Update deprecated trouble pickers

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -6,8 +6,8 @@ function M.setup()
 
    local opts = { noremap = true, silent = true, nowait = false }
    map("n", "<leader>xx", "<cmd>Trouble<cr>", opts)
-   map("n", "<leader>xw", "<cmd>Trouble lsp_workspace_diagnostics<cr>", opts)
-   map("n", "<leader>xd", "<cmd>Trouble lsp_document_diagnostics<cr>", opts)
+   map("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", opts)
+   map("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", opts)
    map("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opts)
    map("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opts)
    -- vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>", opts)
