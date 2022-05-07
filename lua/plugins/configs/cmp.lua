9e0281add8a4358a88b49ed4e1d4d91b7e240afe
Brian Chen <brianchen8990@gmail.com>
2022-05-07 11:22:11 +0200

    Fix cmp sources order

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -93,9 +93,9 @@ local options = {
       }),
    },
    sources = {
+      { name = "nvim_lsp" },
       { name = "cmp_tabnine" },
       { name = "luasnip" },
-      { name = "nvim_lsp" },
       { name = "buffer" },
       { name = "nvim_lua" },
       { name = "path" },
