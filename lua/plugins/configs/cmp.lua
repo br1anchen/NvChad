888b9d3290ad43c663d014e23621f2193f83163f
Brian Chen <brianchen8990@gmail.com>
2022-05-24 16:03:08 +0200

    Use cmp_nvim_lsp capabilities

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -4,7 +4,7 @@ if not present then
    return
 end
 
-vim.opt.completeopt = "menuone,noselect"
+vim.opt.completeopt = "menu,menuone,noselect"
 
 local function border(hl_name)
    return {
--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -44,7 +44,7 @@ function M.on_attach(client, bufnr)
    end
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 capabilities.textDocument.completion.completionItem = {
    documentationFormat = { "markdown", "plaintext" },
