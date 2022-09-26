ac182fe0e133159888281bf33231495ce4e2584c
Brian Chen <brianchen8990@gmail.com>
2022-09-26 15:54:40 +0200

    update octo mapping in whichkey

--- lua/custom/plugins/whichkey.lua
+++ lua/custom/plugins/whichkey.lua
@@ -302,14 +302,37 @@ function M.custom()
 
     o = {
       name = "+Octo",
-      d = { "<cmd>Octo review discard<cr>", " Octo review discard" },
-      m = { "<cmd>Octo pr merge rebase<cr>", "Octo pr merge rebase" },
-      i = { "<cmd>Octo review start<cr>", "Octo review start" },
-      s = { "<cmd>Octo review submit<cr>", "Octo review submit" },
-      a = { "<cmd>Octo review resume<cr>", "Octo review resume" },
-      x = { "<cmd>Octo review close<cr>", "Octo review close" },
-      p = { "<cmd>Octo pr list<cr>", "Octo pr list" },
-      c = { "<cmd>Octo review commit<cr>", "Octo review commits list" },
+      c = {
+        name = "+Comment",
+        a = {
+          "<cmd>Octo comment add<cr>",
+          "Add comment",
+        },
+        d = {
+          "<cmd>Octo comment delete<cr>",
+          "Delete comment",
+        },
+      },
+      p = {
+        name = "+Pull Request",
+        l = { "<cmd>Octo pr list<cr>", "Octo pr list" },
+        m = { "<cmd>Octo pr merge rebase<cr>", "Octo pr merge rebase" },
+        r = { "<cmd>Octo pr reload<cr>", "Octo pr reload" },
+      },
+      r = {
+        name = "+Review",
+        a = { "<cmd>Octo review resume<cr>", "Octo review resume" },
+        c = { "<cmd>Octo review commit<cr>", "Octo review commits list" },
+        d = { "<cmd>Octo review discard<cr>", "Octo review discard" },
+        i = { "<cmd>Octo review start<cr>", "Octo review start" },
+        s = { "<cmd>Octo review submit<cr>", "Octo review submit" },
+        x = { "<cmd>Octo review close<cr>", "Octo review close" },
+      },
+      t = {
+        name = "+Thread",
+        r = { "<cmd>Octo thread resolve<cr>", "Octo thread resolve" },
+        u = { "<cmd>Octo thread unresolve<cr>", "Octo thread unresolve" },
+      },
     },
 
     r = {
