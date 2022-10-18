0ff432f6394b90051a95bbd99974e68525edd3ed
Brian Chen <brianchen8990@gmail.com>
2022-10-18 21:25:40 +0200

    fix: git-worktree post create/switch hook

--- lua/custom/plugins/git-worktree.lua
+++ lua/custom/plugins/git-worktree.lua
@@ -15,35 +15,27 @@ local function is_melos_project()
   return exists "melos.yaml"
 end
 
-local function melos_bs(cmd_path)
-  print 'Melos project detected, running "melos bs"...'
-  local melos_bs_job = Job:new {
-    command = "melos",
-    args = { "bs" },
-    cwd = cmd_path,
-  }
-
-  melos_bs_job:after_success(vim.schedule_wrap(function()
-    print "Melos bootstrap success"
-  end))
-
-  melos_bs_job:after_failure(vim.schedule_wrap(function(j)
-    print("Melos bootstrap failed: " .. j:stderr_result())
-  end))
-
-  melos_bs_job:start()
-end
-
 local M = {}
 
 M.setup = function()
   git_worktree.on_tree_change(function(op, metadata)
-    if op == git_worktree.Operations.Create and is_melos_project() then
-      melos_bs(metadata.path)
-    end
+    if (op == git_worktree.Operations.Create or op == git_worktree.Operations.Switch) and is_melos_project() then
+      print 'Melos project detected, running "melos bs"'
+      local melos_bs_job = Job:new {
+        command = "melos",
+        args = { "bs" },
+        cwd = metadata.path,
+      }
 
-    if op == git_worktree.Operations.Switch and metadata.prev_path ~= metadata.path and is_melos_project() then
-      melos_bs(metadata.path)
+      melos_bs_job:after_success(vim.schedule_wrap(function()
+        print "Melos bootstrap success"
+      end))
+
+      melos_bs_job:after_failure(vim.schedule_wrap(function(j)
+        print("Melos bootstrap failed: " .. j:stderr_result())
+      end))
+
+      melos_bs_job:start()
     end
   end)
 end
