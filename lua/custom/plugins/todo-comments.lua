0a352652df8b7f702bd1a079537a601e496f251f
Brian Chen <brianchen8990@gmail.com>
2022-11-04 08:54:41 +0100

    fix: todo-comment.nvim config

--- lua/custom/plugins/todo-comments.lua
+++ lua/custom/plugins/todo-comments.lua
@@ -2,35 +2,7 @@ local todo = require "todo-comments"
 local M = {}
 
 function M.setup()
-   todo.setup {
-      -- highlighting of the line containing the todo comment
-      -- * before: highlights before the keyword (typically comment characters)
-      -- * keyword: highlights of the keyword
-      -- * after: highlights after the keyword (todo text)
-      highlight = {
-         before = "", -- "fg" or "bg" or empty
-         keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-         after = "fg", -- "fg" or "bg" or empty
-         pattern = [[.*<(KEYWORDS)(\([^\)]*\))?(\s\d*)?:]], -- pattern or table of patterns, used for highlightng (vim regex)
-         comments_only = true, -- uses treesitter to match keywords in comments only
-         max_line_len = 400, -- ignore lines longer than this
-         exclude = {}, -- list of file types to exclude highlighting
-      },
-      search = {
-         command = "rg",
-         args = {
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-         },
-         -- regex that will be used to match keywords.
-         -- don't replace the (KEYWORDS) placeholder
-         pattern = [[\b(KEYWORDS)(\([^\)]*\))?(\s\d*)?:]], -- ripgrep regex
-         -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-      },
-   }
+  todo.setup {}
 end
 
 return M
